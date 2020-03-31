ec8baee148aa66d0fb1f3c63c8254bd1cd54100d
Ryan Marvin <rmarvin1@binghamton.edu>
2020-03-31 17:22:59 -0400

    prints done but does not prompt bash shell

--- ret-to-libc.c
+++ ret-to-libc.c
@@ -5,14 +5,16 @@ extern unsigned long *get_ebp();
 
 void ret2libc(int dummy){
 	/* TASK 1 : Get the value of ebp. Make use of the getebp function you have previously written. */
-	unsigned long *curr_ebp = (unsigned long*) malloc(sizeof(unsigned long));
-	curr_ebp = get_ebp();
+	unsigned long *curr_ebp = (unsigned long*) malloc(sizeof(unsigned long)* 4);
+	curr_ebp[0] = *get_ebp();
 	/* TASK2 : Overwrite written address with address of system. */
+	curr_ebp[1] = (unsigned long) &system;
 	curr_ebp[2] = curr_ebp[1];
-	curr_ebp[1] = &system;
+
 	/* TASK3 : Set up the argument to system as a pointer to string "/bin/bash" */
 	char* str = "/bin/bash";
-	curr_ebp[3] = str;
+	curr_ebp[3] = *str;
+
 }
 
 int main(){
